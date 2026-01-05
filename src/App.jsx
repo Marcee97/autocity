9eafc65dc2e237b9257b14fe404b5c17c12126dd
marcee <marceloquadrilatero@gmail.com>
2026-01-04 22:40:45 -0300

    remove rutasprivadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./ui/Home";
 import { Login } from "./ui/Login";
-import { Privadas } from "../src/components/Privadas";
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-
-        <Route element={<Privadas/>}>
-        
           <Route path="/home" element={<Home />} />
-        
-        </Route>
       </Routes>
     </>
   );
